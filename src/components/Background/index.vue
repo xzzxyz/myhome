ce5ee31771c2a856829c11672b33e40469fe5dff
xzzxyz <xzzxyz@mail.tsinghua.edu.cn>
2026-03-11 16:59:22 +0800

    feat: 更新背景图片API，使用多个稳定的风景图片API作为备用

--- src/components/Background/index.vue
+++ src/components/Background/index.vue
@@ -23,25 +23,39 @@ const changeBg = (type) => {
   if (n > 5) {
     defaultSet()
   } else {
-    fetch('https://api.vvhan.com/api/wallpaper/views')
-      .then(data => data.blob())
-      .then(blob => bgUrl.value = URL.createObjectURL(blob))
-      .then(url => URL.revokeObjectURL(url))
-      .catch(() => {
-        fetch('https://picsum.photos/1920/1080')
-          .then(data => data.blob())
-          .then(blob => bgUrl.value = URL.createObjectURL(blob))
-          .then(url => URL.revokeObjectURL(url))
-          .catch(defaultSet)
-      })
-
-
+    // 尝试多个风景图片API，按优先级顺序
+    const apis = [
+      'https://api.fuchenboke.cn/api/fengjing.php', // 浮沉API - 高清风景壁纸
+      'https://tu.ltyuanfang.cn/api/fengjing.php',   // 远方随机风景壁纸
+      'https://api.dujin.org/pic/fengjing',          // 缙哥哥风景API
+      'https://picsum.photos/1920/1080',             // Picsum随机图片
+    ]
+    
+    // 递归尝试API列表
+    const tryApi = (index) => {
+      if (index >= apis.length) {
+        defaultSet()
+        return
+      }
+      
+      fetch(apis[index])
+        .then(response => {
+          if (!response.ok) throw new Error('API请求失败')
+          return response.blob()
+        })
+        .then(blob => {
+          const url = URL.createObjectURL(blob)
+          bgUrl.value = url
+        })
+        .catch(() => {
+          console.log(`API ${apis[index]} 失败，尝试下一个...`)
+          tryApi(index + 1)
+        })
+    }
+    
+    tryApi(0)
   }
-
-
-
-
-
+}
 
   // if (type == 0) {
   //   bgUrl.value = `/images/background${getRandomNumber(16)}.webp`;
